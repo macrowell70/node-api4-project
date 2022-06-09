3c8eea517e4821c14a5170a7c0152b6d3155562e
unknown <macrowell@yahoo.com>
2022-06-09 19:59:17 -0400

    set up basic endpoints

--- api/server.js
+++ api/server.js
@@ -5,7 +5,19 @@ const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+    res.send('<h1>Welcome To My Application!</h1>')
 })
+
+server.get('/api/users', (req, res) => {
+    res.json({ message: "request has been received" })
+});
+
+server.post('/api/register', (req, res) => {
+    res.json({ message: "request has been received" })
+});
+
+server.post('/api/login', (req, res) => {
+    res.json({ message: "request has been received" })
+});
 
 module.exports = server
