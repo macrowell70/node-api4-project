810483a083ba8472b3f129c3870c37b1fd9c7ce3
unknown <macrowell@yahoo.com>
2022-06-09 19:45:09 -0400

    wired server

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+module.exports = server
